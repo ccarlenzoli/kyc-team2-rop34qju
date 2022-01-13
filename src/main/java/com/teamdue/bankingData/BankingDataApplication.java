d42826855e25f609afc56f9d8f0d5d6bb9605838
Malefica <francescabuso@live.it>
2022-01-13 10:35:52 +0100

    progetto springboot inizializzato e unito con il pom fornito

---
+++ src/main/java/com/teamdue/bankingData/BankingDataApplication.java
@@ -0,0 +1,13 @@
+package com.teamdue.bankingData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamdue/bankingData/BankingDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamdue.bankingData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
