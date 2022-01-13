a03caa723a0aad7c0d2b1364ca463146503134b4
Malefica <francescabuso@live.it>
2022-01-13 15:07:07 +0100

    che problemi ha?!

---
+++ src/main/java/com/teamdue/bankingData/ScheduleTest.java
@@ -0,0 +1,19 @@
+package com.teamdue.bankingData;
+
+import org.junit.platform.commons.logging.Logger;
+import org.junit.platform.commons.logging.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScheduleTest {
+
+    private static final Logger log = LoggerFactory.getLogger(ScheduleTest.class);
+
+    @Scheduled(fixedRate = 3000)
+    public void reportCurrentTime(){
+        System.out.println(
+                System.currentTimeMillis()
+        );
+    }
+}
