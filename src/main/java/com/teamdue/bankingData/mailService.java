0b5d6a246575b8e7fc965954853532a7a5646298
ccarlenzoli <claudio@toptal.com>
2022-01-13 16:58:05 +0100

    fix compilation

--- src/main/java/com/teamdue/bankingData/mailService.java
+++ src/main/java/com/teamdue/bankingData/mailService.java
@@ -1,5 +1,5 @@
 package com.teamdue.bankingData;
 
 public interface mailService {
-    public static void sendEmail(com.teamdue.bankingData.sendMail mail);
+    public void sendEmail(com.teamdue.bankingData.sendMail mail);
 }
